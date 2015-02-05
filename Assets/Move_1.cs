1bc117cbdff2e8663627f4c48323ed17842fa7c9
Querci Luca <lucabit73@gmail.com>
2015-02-05 17:14:06 +0100

    test scene, start player movement

---
+++ Assets/Move_1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_1 : MonoBehaviour {
+
+	public float speed = 3.0F;
+	public float rotateSpeed = 3.0F;
+	public int ang;
+
+	Animator anim;
+	CharacterController controller;
+	Vector3 movement;
+	Quaternion quat;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+		controller = GetComponent<CharacterController>();
+		quat = Quaternion.AngleAxis (ang, Vector3.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// remove from update when ang is set 
+		quat = Quaternion.AngleAxis (ang, Vector3.up);
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * 50 * Time.deltaTime;
+		movement = quat * movement;
+
+
+		controller.SimpleMove(movement);
+		
+		//if (!((h) == 0) || !((v) == 0)) {
+		if ( h != 0f || v != 0f ) {
+			anim.SetBool ("isWalking", true);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Move_0.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_0 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+		Debug.Log (anim.applyRootMotion);
+	}
+
+	public float speed = 3.0F;
+	public float rotateSpeed = 3.0F;
+	Animator anim;
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(Input.GetAxis("Vertical"));
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+
+		if (!(Input.GetAxis ("Vertical") == 0)) {
+			//Debug.Log("asd");
+			anim.SetBool ("isWalking", true);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+
+	}
+}
